3a3196c16c9aeaa7fda328f7b66e23d7aadd4ce0
Ahmet Kargın <ahmet@redmillsolutions.com>
2022-12-07 09:45:37 +0000

    .

--- ErrorMailTypes/Models/MailDto.cs
+++ ErrorMailTypes/Models/MailDto.cs
@@ -6,9 +6,7 @@ namespace ErrorMailTypes.Models
     {
         
         public int id { get; set; }
-       // public string? MailTypeId { get; set; }
         public string? MailType { get; set; }
         public string? MailBody { get; set; }
-        //public List<SelectListItem> MailTypeSelectList { get; set; }
     }
 }
